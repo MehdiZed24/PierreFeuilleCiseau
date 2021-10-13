b24bf2b35daf769945085e2f1913ca67b3ece593
MehdiZed24 <mehdiziani1989@gmail.com>
2021-10-13 16:35:06 +0200

    Exercice fini

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const getUserChoice = (userInput) => {
     userInput === "ciseaux" ||
     userInput === "feuille"
   ) {
+    return userInput;
   } else {
     console.log(
       "Vous n'avez pas saisi un choix possible, merci de saisir 'pierre', 'feuille' ou 'ciseaux"
@@ -14,7 +15,55 @@ const getUserChoice = (userInput) => {
 
 const getComputerChoice = () => {
   let choice = Math.floor(Math.random() * 3);
-  console.log(choice);
+  switch (choice) {
+    case 0:
+      return "pierre";
+      break;
+    case 1:
+      return "feuille";
+      break;
+    case 2:
+      return "ciseaux";
+      break;
+  }
 };
 
-getComputerChoice();
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "Egalité!";
+  }
+  if (userChoice === "pierre") {
+    if (computerChoice === "ciseaux") {
+      return "Vous avez gagné";
+    } else {
+      return "Vous avez perdu";
+    }
+  }
+
+  if (userChoice === "feuille") {
+    if (computerChoice === "pierre") {
+      return "Vous avez gagné";
+    } else {
+      return "Vous avez perdu";
+    }
+  }
+
+  if (userChoice === "ciseaux") {
+    if (computerChoice === "feuille") {
+      return "Vous avez gagné";
+    } else {
+      return "Vous avez perdu";
+    }
+  }
+};
+
+const playGame = () => {
+  userChoice = getUserChoice('pierre'); 
+  console.log(`Vous jouez: ${userChoice}.`);
+  computerChoice = getComputerChoice();
+  console.log(`L'ordinateur joue :  ${computerChoice}.`);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+
+playGame();
